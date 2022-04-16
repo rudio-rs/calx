fea7a37a152a6e4343c92e8d08e45692a5599802
Chun-Min Chang <chun.m.chang@gmail.com>
2022-04-16 10:50:46 -0700

    Reformat

--- src/main.rs
+++ src/main.rs
@@ -16,11 +16,11 @@ fn main() {
     match system_device.get_all_devices() {
         Ok(devices) => {
             fn print_devices_in_scope(devices: &[Device], s: Side) {
-                println!("{} devices:", s);
+                println!("{} devices\n--------------", s);
                 for device in devices.iter() {
                     if device.in_scope(&s).unwrap_or(false) {
                         println!(
-                            "\tid: {}\n\tbuffer frame size range: {}\n\tchannel count: {}\n\tlatency: {}\n\tsample rate: {}\n\tsource: {}\n\ttransport type: {}",
+                            "id: {}\n\tbuffer frame size range: {}\n\tchannel count: {}\n\tlatency: {}\n\tsample rate: {}\n\tsource: {}\n\ttransport type: {}",
                             device.id(),
                             device.buffer_frame_size_range(&s).map_or_else(
                                 |e| format!("Error: {}", e),
